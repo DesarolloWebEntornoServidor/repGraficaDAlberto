c541456096544e8455bea3f178b0592b4678dca4
ALUM25 <ALUM25@PC25>
2017-10-18 10:05:52 +0200

    Agregar archivos de proyecto.

---
+++ RegGraficaDAlberto/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RegGraficaDAlberto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelGeneral = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelGeneral
+            // 
+            this.panelGeneral.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelGeneral.Location = new System.Drawing.Point(189, 61);
+            this.panelGeneral.Name = "panelGeneral";
+            this.panelGeneral.Size = new System.Drawing.Size(627, 322);
+            this.panelGeneral.TabIndex = 0;
+            this.panelGeneral.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGeneral_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 510);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panelGeneral);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGeneral;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RegGraficaDAlberto/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegGraficaDAlberto
+{
+    public partial class Form1 : Form
+    {
+        int ancho, alto, numPuntos = 10, segmentoX;
+
+        Point[] tabPuntos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelGeneral_Paint(object sender, PaintEventArgs e)
+        {
+            ancho = panelGeneral.Width;
+            alto = panelGeneral.Height;
+            tabPuntos = new Point[numPuntos];
+            Random azar = new Random();
+
+            for (int i = 0; i < tabPuntos.Length; i++)
+            {
+                tabPuntos[i] = new Point(i * segmentoX, azar.Next(alto));
+            }
+
+            Pen pen = new Pen(Color.Black, 3);
+
+
+        }
+    }
+}
